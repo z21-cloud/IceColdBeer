8b607206d6f2f6866abdfe2207332de2b4ea12b0
z21 <starostin371@gmail.com>
2026-08-24 18:52:06 +0300

    Added lose hole spawn logic

---
+++ Assets/Scripts/Factories/LoseHoleFactory.cs
@@ -0,0 +1,27 @@
+using IceColdBeer.Core;
+using IceColdBeer.Pools;
+using UnityEngine;
+
+namespace IceColdBeer.Factories
+{
+    public class LoseHoleFactory : IFactory<LoseHole>
+    {
+        private readonly LoseHole _prefab;
+        private readonly Transform _parent;
+        private readonly HolePool _pool;
+
+        public LoseHoleFactory(LoseHole prefab, HolePool pool, Transform parent = null)
+        {
+            _prefab = prefab;
+            _pool = pool;
+            _parent = parent;
+        }
+
+        public LoseHole Create()
+        {
+            var hole = GameObject.Instantiate(_prefab, _parent);
+            // hole doesn't need to be initialized with the pool because it doesn't have any methods that require it. It just needs to be instantiated and placed in the scene.
+            return hole;
+        }
+    }
+}
---
+++ Assets/Scripts/Pools/HolePool.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using IceColdBeer.Core;
+using IceColdBeer.Factories;
+using UnityEngine;
+
+namespace IceColdBeer.Pools
+{
+    public class HolePool : MonoBehaviour
+    {
+        [SerializeField] private LoseHole hole;
+        [SerializeField] private int poolSize;
+        [SerializeField] private Transform parent;
+        [SerializeField] private List<Transform> spawnPoints;
+
+        private ObjectPooling<LoseHole> pool;
+
+        private void Awake()
+        {
+            var factory = new LoseHoleFactory(hole, this, parent);
+            pool = new ObjectPooling<LoseHole>(factory, poolSize);
+
+            if(spawnPoints.Count == 0)
+            {
+                Debug.LogWarning($"[HolePool] No spawn points!");
+            }
+
+            foreach(var spawnPoint in spawnPoints)
+            {
+                var hole = GetHole();
+                if(hole != null)
+                {
+                    hole.transform.position = spawnPoint.position;
+                }
+            }
+        }
+
+        public LoseHole GetHole()
+        {
+            LoseHole hole = pool.Get();
+            if(hole != null) return hole;
+
+            Debug.LogWarning($"[HolePool] Hole Pool is empty, return null!");
+            return null;
+        }
+
+        public void ReleaseHole(LoseHole hole)
+        {
+            pool.Release(hole);
+        }
+    }
+}
