afe091413c77c83808c84765fa3a42b7e7a2b427
z21 <starostin371@gmail.com>
2026-09-22 13:50:24 +0300

    Deleted wrong if-condition in BFS.cs

--- Assets/Scripts/Level/BFS.cs
+++ Assets/Scripts/Level/BFS.cs
@@ -14,8 +14,6 @@ public class BFS
 
     public bool FindPath(Node startNode, Node targetNode, Node[,] nodes)
     {
-        if(nodes == null) return false;
-        
         // Если старт уже совпадает с целью, путь найден сразу
         if(startNode.gridPosition == targetNode.gridPosition)
         {
